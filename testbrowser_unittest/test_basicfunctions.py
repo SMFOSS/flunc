d8c80291d287363a7d1371161202e35b29d806ec
jhammel <jhammel@40f73b84-6177-4a8e-8c61-7eee4b1974ed>
2006-08-28 18:59:36 +0000

    converting over to include the Five.testbrowser case
    git-svn-id: https://svn.openplans.org/svn/topp_ftesting/trunk@992 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- testbrowser_unittest/test_basicfunctions.py
+++ testbrowser_unittest/test_basicfunctions.py
@@ -1,20 +1,40 @@
 import unittest
 
-def test_suite():
-    tests = unittest.TestSuite()
+from metadata import *
 
-    # testfiles should be ordered
-    testfiles = ('op_create_user',
-                 'op_login',
-                 'op_create_project',
-                 'op_edit_project')
+# testfiles should be ordered
+testfiles = ('op_create_user',
+             'op_login',
+             'op_create_project',
+             'op_edit_project')
 
-    for name in testfiles:        
-        Products = __import__("Products.testbrowser_unittest." + name)
-        testmodule = getattr(Products.testbrowser_unittest, name)
-        tests.addTest(testmodule.test_suite())
-              
-    return tests
+if useFiveTB:
+    class TestBasicFunctions(OpenPlansTestCase):
+        'example class for basic tests'
+        
+        def test_dummy(self):
+            print 'hi'
+            print self.portal.absolute_url()
+            browser.open('http://localhost')
+#            browser.open(self.portal.absolute_url())
+
+#            print browser.contents
+
+    def test_suite():
+        print "OpenPlans test:  fivetb"
+        suite = unittest.makeSuite(TestBasicFunctions, 'test')
+        return suite
+else:
+
+    def test_suite():
+        tests = unittest.TestSuite()
+
+        for name in testfiles:
+            Products = __import__("Products.testbrowser_unittest." + name)
+            testmodule = getattr(Products.testbrowser_unittest, name)
+            tests.addTest(testmodule.test_suite())
+
+        return tests
 
 if __name__ == '__main__':
     suite = test_suite()
