1c4458c87337a1c8db20444e26f9eeefe37ec185
rob <rob@40f73b84-6177-4a8e-8c61-7eee4b1974ed>
2007-01-04 22:25:54 +0000

    * use the prepath value if it exists to specify the true path of the zope   object
    git-svn-id: https://svn.openplans.org/svn/flunc/trunk@2231 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- flunc/zope_cleanup.py
+++ flunc/zope_cleanup.py
@@ -9,15 +9,18 @@ __all__ = ['zope_delobject']
 
 def get_twill_var(varname): 
     twill_globals, twill_locals = get_twill_glocals()
-    return twill_globals[varname]
+    return twill_globals.get(varname)
 
 def zope_delobject(container, obj, admin_user, admin_pw): 
-    base_url = get_twill_var('base_url') 
+    base_url = get_twill_var('base_url')
+    prepath = get_twill_var('prepath')
 
     print "(zope) Deleting %s from %s on %s" % (obj, container, base_url)
-    
+
     scheme, uri = urllib.splittype(base_url) 
     host, path = urllib.splithost(uri)
+    if prepath is not None:
+        path = prepath + path
     auth_url = "%s://%s:%s@%s%s" % (scheme, admin_user, admin_pw, host, path)
     portal = XMLRPCServer(auth_url)
     getattr(portal, container).manage_delObjects([obj])
