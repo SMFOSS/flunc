c0424765d09eef83b9a6b06f1ffff8f3257dde67
egj <egj@40f73b84-6177-4a8e-8c61-7eee4b1974ed>
2007-05-23 21:39:23 +0000

    add extension to pull the user id from zope
    git-svn-id: https://svn.openplans.org/svn/flunc/trunk@5645 40f73b84-6177-4a8e-8c61-7eee4b1974ed

---
+++ flunc/zope_get_uid.py
@@ -0,0 +1,31 @@
+import twill 
+from twill.namespaces import get_twill_glocals 
+from xmlrpclib import Server as XMLRPCServer 
+import urllib 
+from twill.commands import go
+
+__all__ = ['get_uid']
+
+
+def get_twill_var(varname): 
+    twill_globals, twill_locals = get_twill_glocals()
+    return twill_globals.get(varname)
+
+def get_uid(username, admin_user, admin_pw): 
+    globals, locals = get_twill_glocals()
+
+    base_url = globals.get('base_url')
+    prepath = globals.get('prepath')
+
+    print "(zope) Getting uid for user %s on %s" % (username, base_url)
+
+    scheme, uri = urllib.splittype(base_url) 
+    host, path = urllib.splithost(uri)
+    if prepath is not None:
+        path = prepath + path
+    auth_url = "%s://%s:%s@%s%s/" % (scheme, admin_user, admin_pw, host, path)
+    portal = XMLRPCServer(auth_url)
+
+    confirmation_code = getattr(portal.portal_memberdata, username).UID()
+
+    locals['__uid__'] = confirmation_code
