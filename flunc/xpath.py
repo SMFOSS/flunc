865d97cf70ef3754c5c0dc9505a3c5561b772c53
rmarianski <rmarianski@40f73b84-6177-4a8e-8c61-7eee4b1974ed>
2008-04-09 22:00:19 +0000

    simplify running find xpath expressions by including the xpath on the find command instead of having to run two commands
    git-svn-id: https://svn.openplans.org/svn/flunc/trunk@15576 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- flunc/xpath.py
+++ flunc/xpath.py
@@ -8,18 +8,15 @@ from lxml.etree import XPathEvalError
 import lxml.html
 import re
 
-__all__ = ['run_xpath', 'find_in_xpath']
+__all__ = ['find_in_xpath']
 
 
-def run_xpath(xpath):
+def _run_xpath(xpath):
     _, twill_locals = get_twill_glocals()
     browser = get_browser()
     html = browser.get_html()
     tree = lxml.html.document_fromstring(html)
 
-    # XXX this raises an error if the xpath is bad
-    # should we catch it more gracefully and print some kind of error message
-    # in flunc to make it more obvious?
     try:
         results = tree.xpath(xpath)
     except XPathEvalError:
@@ -36,25 +33,25 @@ def run_xpath(xpath):
     else:
         log_error("xpath '%s' found no results")
         result = ''
+    # in case we want to cache it at some point
     twill_locals['__xpath_result__'] = result
+    twill_locals['__xpath_expr__'] = xpath
+    return result
 
 #XXX add in argument to pass in flags?
-def find_in_xpath(what):
+def find_in_xpath(what, xpath):
     _, twill_locals = get_twill_glocals()
-    xpath_result = twill_locals.get('__xpath_result__')
-    assert xpath_result is not None, \
-           'Running find_in_xpath without first calling run_xpath'
-
+    xpath_result = _run_xpath(xpath)
     regexp = re.compile(what)
     m = regexp.search(xpath_result)
     if not m:
         raise TwillAssertionError("no match to '%s' in '%s'" %
                                   (what, xpath_result))
-
     if m.groups():
         match_str = m.group(1)
     else:
         match_str = m.group(0)
     twill_locals['__match__'] = match_str
 
-#XXX add a notfind?
+#XXX add a notfind_in_xpath?
+#XXX add a find_last_xpath to make re-running finds on xpaths easier
