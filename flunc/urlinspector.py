264de472c2d19727671a1f870f087cf4f07114b8
egj <egj@40f73b84-6177-4a8e-8c61-7eee4b1974ed>
2009-05-21 16:45:30 +0000

    add urlinspector.url_qs command for making simple assertions about the current url querystring
    git-svn-id: https://svn.openplans.org/svn/flunc/trunk@24170 40f73b84-6177-4a8e-8c61-7eee4b1974ed

---
+++ flunc/urlinspector.py
@@ -0,0 +1,16 @@
+from twill.commands import get_browser
+from twill.errors import TwillAssertionError
+
+import urllib
+
+def url_qs(what):
+    browser = get_browser()
+    qs = urllib.splitquery(browser.get_url())[-1]
+    qs = qs.split('&')
+    qsdict = {}
+    for q in qs:
+        q = q.split('=')
+        qsdict[q[0]] = q[1]
+        
+    if what not in qsdict:
+        raise TwillAssertionError("no match to '%s' in %s" % (what, qs)
